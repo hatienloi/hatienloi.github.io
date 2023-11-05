406bcb02dcf22b62730b7f9005eb35ccb59dfb99
Loi.Ha <hatienloi261299@gmail.com>
2023-11-05 16:50:10 +0700

    feat: change to svelte

---
+++ src/components/main.svelte
@@ -0,0 +1,17 @@
+<script>
+  let src = "/HaTienLoi-CV.pdf";
+</script>
+
+<embed class="main" {src} type="application/pdf" />
+
+<style scoped>
+  .main {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>

2f03feac5c03ff3b1e52d87f615e938fd16e0b52
Loi.Ha <loiht.b17vt220@stu.ptit.edu.vn>
2023-11-05 17:29:25 +0700

    feat: update check device

--- src/components/main.svelte
+++ src/components/main.svelte
@@ -1,17 +1,24 @@
+<script context="module">
+    import Device from 'svelte-device-info'
+</script>
 <script>
-  let src = "/HaTienLoi-CV.pdf";
+    let src = "/HaTienLoi-CV.pdf";
+
+    $: if (Device.isPhone || Device.isMobile || Device.isTablet) {
+        window.location.href = src;
+    }
 </script>
 
-<embed class="main" {src} type="application/pdf" />
+<embed class="main" {src} type="application/pdf"/>
 
 <style scoped>
-  .main {
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-  }
+    .main {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>
